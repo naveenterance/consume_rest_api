35177f52dccb0bdfa2912d1cd92ac63160d7e3dd
Naveen <naveenterance@gmail.com>
2021-09-23 19:10:50 +0530

    default_json

---
+++ src/webpages/user.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect }  from 'react';const User = props => {    var id = props.match.params.id;    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [user, setUser] = useState([]);
+    const [userAddress, setUserAddress] = useState([]);
+    const [userCompany, setUserCompany] = useState([]);
+    
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users/" + id)
+            .then(res => res.json())
+            .then(
+                (data) => {
+                    console.log(data);
+                    setIsLoaded(true);
+                    setUser(data);
+                    setUserAddress(data.address);
+                    setUserCompany(data.company);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, []);    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+    if (!isLoaded) {
+        return <div>Loading...</div>;
+    }  
+    
+    if (user) {
+        return (
+            <div>
+                <h1>{user.name}</h1>
+                <div>
+                    Email: {user.email}
+                </div>
+                <div>
+                    Phone: {user.phone}
+                </div>
+                <div>
+                    Website: {user.website}
+                </div>  
+                <div>
+                    Company: {userCompany.name}
+                </div> 
+                <div>
+                    Address: {userAddress.street}, {userAddress.suite}, {userAddress.city}, {userAddress.zipcode}   
+                </div>  
+
+                
+
+
+                        </div>        );
+    }
+};export default User;
